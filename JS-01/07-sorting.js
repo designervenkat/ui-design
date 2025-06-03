0f6f060bf26b17cee608f4fe0b240f0856131082
@designervenkat <murty.av@gmail.com>
2025-06-03 06:23:21 +0000

    DOM Methods

---
+++ JS-01/07-sorting.js
@@ -0,0 +1,23 @@
+// Sort the elements of an array in place and returns the sorted array
+// By default, it sorts the elements as strings, which can lead to unexpected results when sorting numbers.
+
+// Sorting Arrays String and Numbers
+
+// const fruits = ['Banana', 'Apple', 'Cherry']
+// fruits.sort()
+// console.log(fruits)
+
+// const numbers = [5, 10, 1, 30]
+// // numbers.sort() // not gonna work in numbers
+// numbers.sort((a, b) => a - b)
+// console.log(numbers)
+
+// Sorting Objects
+
+// const people = [
+//     { name: 'Alice', age: 25 },
+//     { name: 'Bob', age: 30 },
+//     { name: 'Charlie', age: 22 },
+// ]
+// people.sort((a, b) => b.age - a.age)
+// console.log(people)
---
+++ JS-01/08-ternary.js
@@ -0,0 +1,26 @@
+// Ternary Operator - short for if...else statements
+
+// Basic syntax :
+
+// condition ? experssion_if_true : experssion_if_false;
+
+// const age = 15
+// const canVote = age >= 18 ? 'Yes, you can vote!' : "No, You can't vote."
+// console.log(canVote)
+
+// const isMember = false
+// const discount = isMember ? 20 : 5
+// console.log(`Discount : ${discount}`)
+
+// Nested Ternary Operator : if ... else if ... else
+// const score = 91
+// const grade = score >= 90 ? 'A' : score >= 75 ? 'B' : 'C'
+// console.log(`Your grade is ${grade}`)
+
+// Using functions
+// function getAccess(role) {
+//     return role === 'admin' ? 'Full Access' : 'Limited Access'
+// }
+
+// console.log(getAccess('user'))
+// console.log(getAccess('admin'))
